e82a4373f0aac569d36cc54c15af8786e20bc263
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-09-28 05:27:29 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 30;
+    if(a < 20){
+        printf("%d is less then 20. \n",a);
+    }else{
+        printf("%d is higher then 20. \n",a);
+    }
+    return 0;
+}
